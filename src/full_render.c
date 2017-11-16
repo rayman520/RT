e248946b29a3f636ea76eff2658807c1b1fd823d
rayman520 <tenyman520@gmail.com>
2017-11-16 10:49:26 +0100

    RT - Ver.19 Build.754

--- src/full_render.c
+++ src/full_render.c
@@ -6,11 +6,27 @@
 /*   By: cpierre <cpierre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/30 14:04:34 by cpierre           #+#    #+#             */
-/*   Updated: 2017/11/14 15:07:07 by cpierre          ###   ########.fr       */
+/*   Updated: 2017/11/16 10:33:44 by cpierre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
+
+static void	save_image(SDL_Surface *img, int i, t_str mapfile)
+{
+	t_str	savefile_name;
+	t_str	tmp;
+
+	savefile_name = ft_strjoin("renders\\RT_Full-render_", mapfile);
+	tmp = ft_strjoin(savefile_name, ft_itoa(i));
+	savefile_name = ft_strjoin(tmp, ".bmp");
+	free(tmp);
+	ft_str_replace(savefile_name, '/', '_');
+	ft_str_replace(savefile_name, '\\', '/');
+	SDL_SaveBMP(img, savefile_name);
+	printf("%s", SDL_GetError());
+	free(savefile_name);
+}
 
 static Uint32 full_render_pixel(t_2dint pos, t_fullmap *map)
 {
@@ -19,7 +35,7 @@ static Uint32 full_render_pixel(t_2dint pos, t_fullmap *map)
 	return(0xffffff);
 }
 
-static void full_render_images(t_SDL_Bundle b, t_fullmap *map)
+static void full_render_images(t_SDL_Bundle b, t_fullmap *map, t_str mapfile)
 {
 	t_2dint	pos;
 	int i;
@@ -41,27 +57,11 @@ static void full_render_images(t_SDL_Bundle b, t_fullmap *map)
 			sub_put_percent(b.render_win, b.window_img, 100 * (double)(pos.y + i * b.render_img->h) / (b.render_img->h * image_number));
 		}
 		SDL_UnlockSurface(b.render_img);
-		save_image(render_img);
-		sub_blit_render(render_img, window_img);
-		SDL_UpdateWindowSurface(render_win);
+		save_image(b.render_img, i, mapfile);
+		sub_blit_render(b.render_img, b.window_img);
+		SDL_UpdateWindowSurface(b.render_win);
 		i++;
 	}
-}
-
-static void	save_image(SDL_Surface *img)
-{
-	t_str	savefile_name;
-	t_str	tmp;
-
-	savefile_name = ft_strjoin("renders\\RT_Full-render_", mapfile);
-	tmp = ft_strjoin(savefile_name, ft_itoa(i));
-	savefile_name = ft_strjoin(tmp, ".bmp");
-	free(tmp);
-	ft_str_replace(savefile_name, '/', '_');
-	ft_str_replace(savefile_name, '\\', '/');
-	SDL_SaveBMP(img, savefile_name);
-	printf("%s", SDL_GetError());
-	free(savefile_name);
 }
 
 void	full_render_start(t_str mapfile)
@@ -69,14 +69,17 @@ void	full_render_start(t_str mapfile)
 	SDL_Window	*render_win;
 	SDL_Surface *window_img;
 	SDL_Surface *render_img;
+	SDL_version	ver;
 	t_fullmap	*map;
 
+	SDL_GetVersion(&ver);
+	printf("%d.%d.%d\n", ver.major, ver.minor, ver.patch);
 	map = parser(mapfile);
 	mkdir("renders", 0777);
 	render_win = sub_create_render_window(mapfile,
 			RENDER_WIN_WIDTH, RENDER_WIN_HEIGHT);
 	window_img = SDL_GetWindowSurface(render_win);
 	render_img = SDL_CreateRGBSurface(0, map->res.x, map->res.y, 32, 0, 0, 0, 0);
-	full_render_images((t_SDL_Bundle){render_win, window_img, render_img}, map);
+	full_render_images((t_SDL_Bundle){render_win, window_img, render_img}, map, mapfile);
 	sub_fullrender_end(render_win, window_img);
 }
