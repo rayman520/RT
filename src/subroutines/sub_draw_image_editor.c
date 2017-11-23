de5dc4d0123bf70ffb9202a2e54ad9096abc780a
Nicolas THIBAUDAULT <nthibaud@e1r5p10.42.fr>
2017-11-23 11:13:04 +0100

    modif draw_editor and fullrender  to test matrix

--- src/subroutines/sub_draw_image_editor.c
+++ src/subroutines/sub_draw_image_editor.c
@@ -6,7 +6,7 @@
 /*   By: cpierre <cpierre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/29 15:57:29 by cpierre           #+#    #+#             */
-/*   Updated: 2017/11/23 10:17:07 by nthibaud         ###   ########.fr       */
+/*   Updated: 2017/11/23 11:11:20 by nthibaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,12 +35,44 @@ static t_vect calc_pix_vect(t_cam_vects cam, t_2dint pos, SDL_Surface *img)
 	out = ft_unit_vect(out);
 	return (out);
 }
+/*
+void	pscreen_to_pcam(SDL_Surface *img, t_3d_double *v)
+{
+	v->x = (2 * ((v->x + 0.5) / img->w) - 1) * tan(0.52 / 2);
+	v->y = (1 - 2 * ((v->y + 0.5) / img->h)) * tan(0.52 / 2);
+	if (img->w > img->h)
+		v->x = v->x * img->w / img->h;
+	else if (img->w < img->h)
+		v->y = v->y * img->h / img->w;
+}
 
+void	build_c2w(double **c2w, t_vect cam)
+{
+	t_3d_double	tmp;
+	t_3d_double forward;
+	t_3d_double	right;
+	t_3d_double	up;
+	t_m_rows	rows;
+
+	tmp = (t_3d_double){0,1,0};
+	forward = v_norm(v_sub_a_by_b(cam.pos, cam.dir));
+	right = v_cross(tmp, forward);
+	up = v_cross(forward, right);
+	rows.x_row = forward;
+	rows.y_row = right;
+	rows.z_row = up;
+	rows.w_row = (t_3d_double){0,0,0};
+	m4_fill(c2w, rows);
+}
+*/
 void	sub_draw_image_editor(t_fullmap *map, SDL_Surface *img)
 {
+//	double		**c2w;
 	t_2dint		c;
 	t_vect		pix_vect;
 
+//	m4_allocate(c2w);
+//	build_c2w(c2w, map->camera[map->target_cam]);
 	map->cam_v = sub_calc_cam_vects(map->camera[map->target_cam], img->w, img->h);
 	map->cam_v.fov = map->fov;
 	display_cam_vects("cam_v", map->cam_v);
--- src/subroutines/sub_primary_ray.c
+++ src/subroutines/sub_primary_ray.c
@@ -6,7 +6,7 @@
 /*   By: nthibaud <nthibaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/07 13:06:49 by nthibaud          #+#    #+#             */
-/*   Updated: 2017/11/23 10:43:37 by nthibaud         ###   ########.fr       */
+/*   Updated: 2017/11/23 11:11:31 by nthibaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,28 @@ void	fill_c2w_matrix(double **m, t_v3d from, t_v3d to)
 	x = v_cross(tmp, z);
 	y = v_cross(z, x);
 	w = from;
-m_row_fill(m, x, 0, 0);
+	m_row_fill(m, x, 0, 0);
 	m_row_fill(m, y, 1, 0);
 	m_row_fill(m, z, 2, 0);
 	m_row_fill(m, w, 3, 1);
+}
+
+void	build_c2w(double **c2w, t_vect cam)
+{
+	t_3d_double	tmp;
+	t_3d_double forward;
+	t_3d_double	right;
+	t_3d_double	up;
+	t_m_rows	rows;
+
+	tmp = (t_3d_double){0,1,0};
+	forward = v_norm(v_sub_a_by_b(cam.pos, cam.dir));
+	right = v_cross(tmp, forward);
+	up = v_cross(forward, right);
+	rows.x_row = forward;
+	rows.y_row = right;
+	rows.z_row = up;
+	rows.w_row = (t_3d_double){0,0,0};
 }
 
 void	pscreen_to_pcam(SDL_Surface *img, t_3d_double *v)
@@ -46,7 +64,10 @@ void	cast_primary_rays(t_fullmap *map, t_SDL_Bundle b)
 	t_v3d	p;
 	t_ray	ray;
 	t_2dint	pos;
+	double		**c2w;
 
+	m4_allocate(c2w);
+	build_c2w(c2w, map->camera[map->target_cam]);
 	pos.y = -1;
 	r_set_to_zero(&ray);
 	ray.o.v = m_x_v(e->c2w, ray.o.v);
