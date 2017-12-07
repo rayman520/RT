333f91cf0f4bc083e2535b42374dec2d073f6549
Matteo ULZEGA <mulzega@e1r5p18.42.fr>
2017-12-07 15:06:33 +0100

    Cartoon filter

--- src/raytrace_fullrender.c
+++ src/raytrace_fullrender.c
@@ -6,17 +6,31 @@
 /*   By: nthibaud <nthibaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 12:06:07 by nthibaud          #+#    #+#             */
-/*   Updated: 2017/12/07 11:03:11 by nthibaud         ###   ########.fr       */
+/*   Updated: 2017/12/06 17:53:06 by bvan-dyc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 
+static int ft_shade(int i)
+{
+	if (i <= 10)
+		return(0);
+	if (i > 10 && i <= 60)
+		return(41);
+	if (i > 60 && i <= 254)
+		return(150);
+	if (i > 254)
+		return(255);
+	else
+		return(1);
+}
+
 static void	rt_filter(t_fullmap *map, t_3d_double *rescolor)
 {
 	t_3d_double tmp;
 
-	map->filter = NOFILTER;
+	map->filter = CARTOON;
 	tmp.x = rescolor->x;
 	tmp.y = rescolor->y;
 	tmp.z = rescolor->z;
@@ -39,6 +53,12 @@ static void	rt_filter(t_fullmap *map, t_3d_double *rescolor)
 		rescolor->y = (tmp.x * 0.349) + (tmp.y * 0.686) + (tmp.z * 0.168);
 		rescolor->z = (tmp.x * 0.272) + (tmp.y * 0.534) + (tmp.z * 0.131);
 	}
+	if (map->filter == CARTOON)
+	{
+		rescolor->x = ft_shade(rescolor->x);
+		rescolor->y = ft_shade(rescolor->y);
+		rescolor->z = ft_shade(rescolor->z);
+	}
 }
 
 t_3d_double	raytrace_loop(t_fullmap *map, t_vect ray, int depth)
@@ -47,14 +67,13 @@ t_3d_double	raytrace_loop(t_fullmap *map, t_vect ray, int depth)
 	t_hit		hit;
 
 	color = (t_3d_double){0,0,0};
+	map->maxdepth = 3;
 	if (depth < map->maxdepth)
 	{
 		hit = sub_inter_objects(map, ray);
 		if (hit.is_hit == 1 && map->light_c > 0)
 			color = sub_light_primary_ray(map, hit, &ray, depth);
 	}
-	else if (depth >= map->maxdepth)
-		printf("\n\n END LOOP RECURS\n\n");
 	return (color);
 }
 
