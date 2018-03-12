fb9a7c790017bfeda5fc9bc599cea4e4ba21d7a5
Nicolas THIBAUDAULT <nthibaud@e1r6p17.42.fr>
2018-03-12 16:54:04 +0100

    norm

--- src/basic/ft_check_tag_ception.c
+++ src/basic/ft_check_tag_ception.c
@@ -6,13 +6,13 @@
 /*   By: cpierre <cpierre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/12 15:48:26 by cpierre           #+#    #+#             */
-/*   Updated: 2018/03/12 15:54:36 by cpierre          ###   ########.fr       */
+/*   Updated: 2018/03/12 16:53:55 by nthibaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 
-void ft_check_tag_ception(const char *name)
+void	ft_check_tag_ception(const char *name)
 {
 	if (!ft_strcmp(name, "object") || !ft_strcmp(name, "light") ||
 		!ft_strcmp(name, "camera") || !ft_strcmp(name, "objects") ||
