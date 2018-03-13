d4f5cce860d421d4bff5e5bdb9e7359f99376ecf
Nicolas THIBAUDAULT <nthibaud@e1r6p17.42.fr>
2018-03-13 20:18:53 +0100

    norm

--- src/basic/ft_str_is_double_fmt.c
+++ src/basic/ft_str_is_double_fmt.c
@@ -6,13 +6,13 @@
 /*   By: cpierre <cpierre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/13 18:38:57 by cpierre           #+#    #+#             */
-/*   Updated: 2018/03/13 19:03:39 by cpierre          ###   ########.fr       */
+/*   Updated: 2018/03/13 20:18:46 by nthibaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 
-int ft_str_is_double_fmt(t_str str)
+int	ft_str_is_double_fmt(t_str str)
 {
 	while (str && *str)
 	{
