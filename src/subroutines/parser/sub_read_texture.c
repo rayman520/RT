acaae966aa2833e24550a87913b7ae17727891e7
rayman520 <tenyman520@gmail.com>
2018-03-13 19:39:35 +0100

    fix

--- src/subroutines/parser/sub_read_texture.c
+++ src/subroutines/parser/sub_read_texture.c
@@ -6,7 +6,7 @@
 /*   By: cpierre <cpierre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/20 14:56:42 by cpierre           #+#    #+#             */
-/*   Updated: 2018/03/13 19:30:52 by cpierre          ###   ########.fr       */
+/*   Updated: 2018/03/13 19:39:21 by cpierre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ SDL_Surface	*sub_read_texture(t_str file)
 
 	if (file == NULL || ft_strlen(file) < 1)
 		ft_exit("err parser");
+	if (!ft_strcmp(file, "none"))
+		return (NULL);
 	out = IMG_Load(file);
 	if (!out)
 		return (NULL);
