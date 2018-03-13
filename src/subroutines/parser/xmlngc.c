fd19a1577b3f5be945981aa710cfbcfe76da39d5
rayman520 <tenyman520@gmail.com>
2018-03-13 21:05:57 +0100

    fix

--- src/subroutines/parser/xmlngc.c
+++ src/subroutines/parser/xmlngc.c
@@ -6,7 +6,7 @@
 /*   By: cpierre <cpierre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 15:35:50 by cpierre           #+#    #+#             */
-/*   Updated: 2018/03/13 20:56:56 by cpierre          ###   ########.fr       */
+/*   Updated: 2018/03/13 21:05:43 by cpierre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,11 @@ char		*xmlngc(const xmlNode *cur)
 {
 	t_str str;
 
-	ft_hey();
 	if (cur->children && !ft_strcmp((const char *)cur->children->name, "text"))
 	{
 		str = (char *)xmlNodeGetContent(cur);
 		if (!str || ft_strlen(str) < 1)
 			ft_exit("this is our last hope");
-		printf("%s\n", str);
 		return (str);
 	}
 	else
