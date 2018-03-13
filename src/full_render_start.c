bdabf7eb676fdb2b9c8fd7065f16ea9071828ae5
Nicolas THIBAUDAULT <nthibaud@e1r6p17.42.fr>
2018-03-13 14:53:17 +0100

    del printf

--- src/full_render_start.c
+++ src/full_render_start.c
@@ -6,7 +6,7 @@
 /*   By: nthibaud <nthibaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/01 16:28:54 by nthibaud          #+#    #+#             */
-/*   Updated: 2018/03/05 15:08:42 by nthibaud         ###   ########.fr       */
+/*   Updated: 2018/03/13 14:53:04 by nthibaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,20 +36,13 @@ void		full_render_start(t_sdl_bundle b, t_fullmap *map, t_str mapfile)
 {
 	int		i;
 	int		image_number;
-	Uint32	time_before;
-	Uint32	time_after;
-	Uint32	time;
 
 	image_number = 1;
 	i = 0;
 	while (i < image_number)
 	{
 		SDL_LockSurface(b.render_img);
-		time_before = SDL_GetTicks();
 		full_render_threads(map, b);
-		time_after = SDL_GetTicks();
-		time = time_after - time_before;
-		printf("FULLRENDER TIME : %d.%d sec\n", time / 1000, time % 1000);
 		SDL_UnlockSurface(b.render_img);
 		save_image(b.render_img, i, mapfile);
 		sub_blit_render(b.render_img, b.window_img);
--- src/raytrace_editor.c
+++ src/raytrace_editor.c
@@ -6,7 +6,7 @@
 /*   By: cpierre <cpierre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/04 16:24:21 by cpierre           #+#    #+#             */
-/*   Updated: 2018/03/09 15:59:37 by nthibaud         ###   ########.fr       */
+/*   Updated: 2018/03/13 14:52:18 by nthibaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ t_ui			raytrace_editor(t_fullmap *map, t_vect ray)
 	t_3d_double	color;
 
 	color = raytrace_loop(map, ray, 0);
-	ft_rt_filter(map, &color);
+	if (map->filter != 0)
+		ft_rt_filter(map, &color);
 	return ((unsigned int)ft_double_3d_to_int(color));
 }
